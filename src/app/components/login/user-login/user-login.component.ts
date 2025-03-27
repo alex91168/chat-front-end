71ee0611693e26275ad1725c372190fd2fce9559
Alex <alexbaatista93@gmail.com>
2025-03-26 21:35:52 -0300

    forms

--- src/app/components/login/user-login/user-login.component.ts
+++ src/app/components/login/user-login/user-login.component.ts
@@ -1,14 +1,29 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.scss'
 })
 export class UserLoginComponent {
-  
+  constructor( private formBuild: FormBuilder ){}
   @Output() setUserInput = new EventEmitter<string>();
+  loginForm!: FormGroup; 
+
+  ngOnInit(): void{
+    this.loginForm = this.formBuild.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  formSubmit(): void {
+    if (!this.loginForm.valid) throw new Error("Formulário inválido.")
+    const formValue = this.loginForm.value;
+    console.log("Enviado", formValue)
+  }
 
   clickRegister(): void {
     this.setUserInput.emit("register");
--- src/app/components/login/user-register/user-register.component.ts
+++ src/app/components/login/user-register/user-register.component.ts
@@ -1,14 +1,31 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-register.component.html',
   styleUrl: './user-register.component.scss'
 })
 export class UserRegisterComponent {
+  constructor ( private formBuild: FormBuilder){}
   @Output() setUserInput = new EventEmitter<string>();
+  formRegister!: FormGroup;
 
+  ngOnInit(): void {
+    this.formRegister = this.formBuild.group({
+      user: [''],
+      password: [''],
+      repassword: [''],
+      email: ['']
+    })
+  }
+
+  formSubmit(): void {
+    if (!this.formRegister.valid) throw new Error("Formulário inválido.")
+    const formRegister = this.formRegister.value;
+    console.log("Formulario de registro", formRegister)
+  }
   clickLogin(): void {
     this.setUserInput.emit("login");
   }
